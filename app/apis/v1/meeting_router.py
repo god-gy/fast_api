8245174d4d5ad4f9cd82ee9aeb04510ae57358ba
god-gy <god.gy0321@gmail.com>
2025-11-29 15:32:43 +0900

    router prefix 오타 수정

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -4,7 +4,7 @@ from app.dtos.create_meeting_response import CreateMeetingResponse
 from app.service.meeting_service_mysql import service_create_meeting_mysql
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
-mysql_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
+mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
 # 원래는 어떤 db를 쓰는지 url에 적을 필요 없음.
 # 강의에서만 이렇게 하는거임
 
